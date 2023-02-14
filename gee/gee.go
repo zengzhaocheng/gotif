0ece74b68d938dfc6e2f714ab06d1da0554dca3c
jackgnez <zengand47@gmail.com>
2023-02-14 11:31:03 +0800

    更新gee.go
    返回状态码

--- gee/gee.go
+++ gee/gee.go
@@ -38,11 +38,13 @@ func (engine *Engine) Run(addr string) (err error) {
 	return http.ListenAndServe(addr, engine)
 }
 
+// ServerHTTP事在http库里定义的，实现了这个接口的struct engine 是可以作为接口传入的
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	key := req.Method + "-" + req.URL.Path
 	if handler, ok := engine.router[key]; ok {
 		handler(w, req)
 	} else {
+		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
 	}
 }
